1e1186130d8448835adc2ea9e483f6da19910884
cbvcode <cbvcode@gmail.com>
2025-06-06 18:20:55 +0300

    Updates media collection configuration and types
    Modifies the Media collection to enhance access control by specifying create, delete, and update permissions for authenticated users, while allowing anyone to read. Additionally, updates the Media interface to make the 'alt' property optional, improving flexibility in media management. Also, adds 'media' to .gitignore to exclude media files from version control.

--- src/app/(payload)/collections/Media.ts
+++ src/app/(payload)/collections/Media.ts
@@ -1,15 +1,29 @@
 import type { CollectionConfig } from 'payload'
 
+import { anyone, authenticated } from '../service/access.service'
+
+// media
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
-    read: () => true,
+    create: authenticated,
+    delete: authenticated,
+    read: anyone,
+    update: authenticated,
+  },
+  admin: {
+    defaultColumns: ['filename', 'mimeType', 'size'],
+    useAsTitle: 'filename',
+  },
+  labels: {
+    singular: 'Media',
+    plural: 'Media Storage',
   },
   fields: [
     {
       name: 'alt',
       type: 'text',
-      required: true,
+      defaultValue: 'Image',
     },
   ],
   upload: true,
